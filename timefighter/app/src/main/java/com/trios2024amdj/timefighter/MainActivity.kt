530f8a8e3d0d3b7b970999de987f0aef1942fc36
Douglas Jasper <doug.jasper@trios.com>
2024-07-30 20:32:59 -0400

    End of Chapter 3

--- timefighter/app/src/main/java/com/trios2024amdj/timefighter/MainActivity.kt
+++ timefighter/app/src/main/java/com/trios2024amdj/timefighter/MainActivity.kt
@@ -1,20 +1,88 @@
 package com.trios2024amdj.timefighter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapMeButton: Button
+
+    private var score = 0
+
+    private var gameStarted = false
+
+    private lateinit var countDownTimer: CountDownTimer
+    private var initialCountDown: Long = 60000
+    private var countDownInterval: Long = 1000
+    private var timeLeft = 60
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        gameScoreTextView = findViewById(R.id.game_score_text_view)
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+        tapMeButton = findViewById(R.id.tap_me_button)
+
+        tapMeButton.setOnClickListener { incrementScore() }
+
+        resetGame()
+
     }
+
+    private fun incrementScore() {
+        if (!gameStarted) {
+            startGame()
+        }
+
+        score++ // score = score + 1
+
+        val newScore = getString(R.string.your_score, score)
+        gameScoreTextView.text = newScore
+
+    }
+
+    private fun resetGame() {
+        score = 0
+
+        val initialScore = getString(R.string.your_score, score)
+        gameScoreTextView.text = initialScore
+
+        val initialTimeLeft = getString(R.string.time_left, 60)
+        timeLeftTextView.text = initialTimeLeft
+
+        countDownTimer = object: CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeft = millisUntilFinished.toInt() / 1000
+
+                val timeLeftString =  getString(R.string.time_left, timeLeft)
+                timeLeftTextView.text = timeLeftString
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+
+        }
+        gameStarted = false
+    }
+
+    private fun startGame() {
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    private fun endGame() {
+        Toast.makeText(this, getString(R.string.game_over_message, score),
+            Toast.LENGTH_LONG).show()
+        resetGame()
+    }
+
 }
